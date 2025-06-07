deef06f232b5ad565714c8e6a0293b307c84d8a4
J6N6S6 <jonasscandeias@gmail.com>
2025-06-06 22:21:47 -0300

    fix> build files

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,31 @@
-// src/app.module.ts
 import { Module } from '@nestjs/common';
-import { ConfigModule } from '@nestjs/config';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       isGlobal: true,
+      envFilePath: ['.env'],
+    }),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (config: ConfigService) => ({
+        type: 'postgres',
+        host: config.get<string>('TYPEORM_HOST'),
+        port: config.get<number>('TYPEORM_PORT'),
+        username: config.get<string>('TYPEORM_USERNAME'),
+        password: config.get<string>('TYPEORM_PASSWORD'),
+        database: config.get<string>('TYPEORM_DATABASE'),
+        entities: [__dirname + '/**/*.entity{.ts,.js}'],
+        synchronize: false,
+        retryAttempts: 5,
+        retryDelay: 3000,
+        ssl: {
+          rejectUnauthorized: false,
+        },
+      }),
     }),
   ],
 })
